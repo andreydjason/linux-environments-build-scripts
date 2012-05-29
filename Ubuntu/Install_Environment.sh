be294964359c7ec0080ad8b77ccb1d41cb439da6
Andrey Djason Viana <andreydjason@gmail.com>
2012-05-29 17:02:19 -0300

    fix

--- Ubuntu/Install_Environment.sh
+++ Ubuntu/Install_Environment.sh
@@ -11,9 +11,6 @@
 # Installs all dependencies, including:
 # Ruby, Rails, Percona Server 5.5 (uses MySQL), PostgreSQL, PHP5, PHPMyAdmin, Apache2 and some other thinks, see it above.
 
-# Install MySQL
-sudo apt-get install mysql-common mysql-client mysql-server
-
 # Install Server and PHP/Ruby/Rails/MySQL/PostgreSQL Requirements
 sudo apt-get install make gcc build-essential curl openssl zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev ncurses-dev automake libtool bison subversion libmysql-ruby libncurses5-dev ruby1.8-dev ruby1.9.1-dev lynx lynx-cur python-software-properties rar unrar-free postgresql postgresql-server-dev-8.4
 
@@ -33,11 +30,10 @@ sudo apt-get install usb-imagewriter usb-creator-gtk gparted
 
 # Percona Server (uses MySQL but with a significant gain in performance)
 gpg --keyserver  hkp://keys.gnupg.net --recv-keys 1C4CBDCDCD2EFD2A
+gpg -a --export CD2EFD2A | sudo apt-key add -
 sudo add-apt-repository "deb http://repo.percona.com/apt lenny main"
-sudo add-apt-repository "deb-src http://repo.percona.com/apt lenny main"
 sudo apt-get update
-sudo apt-get install libmysqlclient-dev libaio-dev
-sudo apt-get install percona-server-common-5.1 percona-server-client-5.1 percona-server-server-5.1
+sudo apt-get install percona-server-server-5.1 percona-server-client-5.1
 
 # Install Apache
 sudo apt-get install apache2 apache2.2-common apache2-mpm-prefork apache2-utils apache2-prefork-dev
